abc8aafe250f5836d767fd6d6e09ba1f22ea3039
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-10-28 13:17:35 +0000

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import Layout from '../components/shared/layout/Layout'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+
+
+  return (
+  
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+  )
+
+
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>
+          Tech site
+        </title>
+      
+      </Head>
+
+      <main>
+
+      
+      </main>
+    </div>
+  );
+}
