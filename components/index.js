7cc1c1131d673e5de282e77c1d87fb6638bb052d
estherlardze <estherlardze8@gmail.com>
2023-10-31 17:22:55 +0000

    working on home

--- components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as DropdownA} from './DropDownA';
-export {default as DropdownB} from './DropDownB';
-export {default as DropdownC} from './DropDownC';
-export {default as Cart} from './Cart';
-export {default as HomeHero} from './HomeHero';
