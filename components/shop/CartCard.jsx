80b58b48e0ce20530d694b91cb1e7f8886362e4f
Bankah Anthony Bekoe <107210739+bankah-junior@users.noreply.github.com>
2023-11-03 21:16:14 +0000

    Worked on the shop details page

--- components/shop/CartCard.jsx
+++ components/shop/CartCard.jsx
@@ -4,12 +4,12 @@ import { HeadingTag } from '../home4'
 import { FaShoppingCart } from 'react-icons/fa'
 import Image from 'next/image'
 
-const CartCard = ({ ProductLink, CategoryLink, CartImg, CartTag, CartTitle, CartPrice, CartKey, }) => {
+const CartCard = ({ ShopLink, CategoryLink, CartImg, CartTag, CartTitle, CartPrice, CartKey, }) => {
   return (
     <>
     <div className='space-y-8' key={CartKey}>
         <div className="relative">
-            <Link href={`/product/` + ProductLink} className='overflow-hidden'><Image src={CartImg} className='transition-transform duration-150 ease-linear rounded hover:scale-105' /></Link>
+            <Link href={`/shop/` + ShopLink} className='overflow-hidden'><Image src={CartImg} className='transition-transform duration-150 ease-linear rounded hover:scale-105' /></Link>
             <Link href={`/category/` + CategoryLink.toLowerCase()}>
             <HeadingTag 
                 extraStyle={"absolute top-5 left-5 hover:text-black cursor-pointer"} 
@@ -19,7 +19,7 @@ const CartCard = ({ ProductLink, CategoryLink, CartImg, CartTag, CartTitle, Cart
         </div>
         <div className="flex flex-row items-start justify-between">
             <div className='space-y-2'>
-            <h2 className="text-2xl font-bold transition-colors duration-300 ease-linear md:text-2xl hover:text-purple-800"><Link href={`/product/` + ProductLink}>{CartTitle}</Link></h2>
+            <h2 className="text-2xl font-bold transition-colors duration-300 ease-linear md:text-2xl hover:text-purple-800"><Link href={`/shop/` + ShopLink}>{CartTitle}</Link></h2>
             <p className='font-semibold'>$ {CartPrice} USD</p>
             </div>
             <button  className='inline-flex items-center p-4 bg-[#8000ff] rounded-full text-white text-lg border-2 border-[#8000ff] transition-colors ease-linear duration-100 hover:bg-white hover:text-black space-x-8'>
