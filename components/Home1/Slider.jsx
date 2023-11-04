fffbca15fab2a44c4625aa65e276a12f5ef3a8c5
estherlardze <estherlardze8@gmail.com>
2023-11-04 20:25:01 +0000

    add slider

--- components/Home1/Slider.jsx
+++ components/Home1/Slider.jsx
@@ -1,18 +1,34 @@
 import {TestimonialCard} from '../links'
 import SliderCss from '../../styles/Slider.module.css';
-import { TestimonialDB } from '../home4/slider/testimonialsDB';
+import { TestimonialDB } from './index';
 
 const Slider = () => {
   return (
-    <div className="overflow-hidden relative w-full ">
-        <div className={`${SliderCss.slideOnMobile} ${SliderCss.slideTop} space-x-10 lg:grid lg:grid-cols-2 lg:gap-8`}>
-            {
-              TestimonialDB.map((TDB, index) => (
-                 <div className={`grid w-full place-content-center lg:col-span-1`} key={index}>
-                    <TestimonialCard message={TDB.message} name={TDB.name} />
-                 </div>
-                ))
-            }
+    <div className="overflow-hidden relative w-full z-20 lg:h-[100vh]">
+      <div className={`space-x-4 flex items-center lg:gap-4 ${SliderCss.slideOnMobile}`}>
+        <div className={`flex fle-row lg:flex-col gap-4 ${SliderCss.slideTop}`}>
+          <div >
+            <TestimonialCard message="I'm thrilled with Tech's services! Their team's in-depth understanding of our industry allowed them to create a software solution that not only met but exceeded our demands, resulting in improved efficiency and growth." name="Sophia Jackson" />
+          </div>
+          <div>
+          <TestimonialCard message="I'm thrilled with Tech's services! Their team's in-depth understanding of our industry allowed them to create a software solution that not only met but exceeded our demands, resulting in improved efficiency and growth." name="Sophia Jackson" />
+          </div>
+          <div >
+          <TestimonialCard message="I'm thrilled with Tech's services! Their team's in-depth understanding of our industry allowed them to create a software solution that not only met but exceeded our demands, resulting in improved efficiency and growth." name="Sophia Jackson" />
+          </div>
+        </div>
+
+        <div className={`flex flex-row lg:flex-col gap-4 ${SliderCss.slidebottom} `}>
+          <div >
+          <TestimonialCard message="I'm thrilled with Tech's services! Their team's in-depth understanding of our industry allowed them to create a software solution that not only met but exceeded our demands, resulting in improved efficiency and growth." name="Sophia Jackson" />
+          </div>
+          <div >
+          <TestimonialCard message="I'm thrilled with Tech's services! Their team's in-depth understanding of our industry allowed them to create a software solution that not only met but exceeded our demands, resulting in improved efficiency and growth." name="Sophia Jackson" />
+          </div>
+          <div >
+          <TestimonialCard message="I'm thrilled with Tech's services! Their team's in-depth understanding of our industry allowed them to create a software solution that not only met but exceeded our demands, resulting in improved efficiency and growth." name="Sophia Jackson" />
+          </div>
+        </div>
         </div>
     </div>
   )
